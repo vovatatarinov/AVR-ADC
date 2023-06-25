08f4804773425b388accda31eafc280d81120446
vovatatarinov <vovatatarinov@bk.ru>
2023-06-25 12:42:43 +0900

    Disable receiver in UART

--- main.c
+++ main.c
@@ -23,8 +23,8 @@ void UART_Init() {
   UCSR0A |= (1 << U2X0); //set x2 speed for decreasing clock errors
   //Maximum clocking error is -3.5% for 230.4k baud rate for Fosc =  16.0000MHz (Arduino default oscillator)
   //You can set baud rate up to 2 Mbps with Fosc =  16.0000MHz
-  /*Enable receiver and transmitter */
-  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+  /*Disable receiver. Enable transmitter */
+  UCSR0B = (0<<RXEN0) | (1<<TXEN0);
   /* Set frame format: 8N1 */
   UCSR0C = 0b00000110;
 }
@@ -48,37 +48,6 @@ void UART_puts_p(const char* s) {
     // transmit character until NULL is reached
     while(pgm_read_byte(s) != 0)
       UART_putc(pgm_read_byte(s++));
-}
-
-unsigned char UART_getc(void) {
-    // wait for data
-    while(!(UCSR0A & (1 << RXC0)));
-    // return data
-    return UDR0;
-}
-
-void UART_Flush( void ) {
-  unsigned volatile char dummy;
-  _delay_ms(1);
-  while ( UCSR0A & (1<<RXC0) ) { dummy = UDR0; _delay_ms(1); }
-  dummy += 0; //Warning
-}
-
-void UART_getLine(char* buf, uint8_t n) {
-    uint8_t bufIdx = 0;
-    char c;
-    // while received character is not carriage return or newline symbol
-    // and end of buffer has not been reached
-    do
-    {
-        // receive character
-        c = UART_getc();
-        // store character in buffer
-        buf[bufIdx++] = c;
-    }
-    while((bufIdx < n) && (c != '\r') && (c != '\n'));
-    // ensure buffer is null terminated
-    buf[bufIdx] = 0;
 }
 
 
