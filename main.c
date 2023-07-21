4c9c4b49e6be1d5a4ed41d00f276d09fe914db0e
vovatatarinov <vovatatarinov@bk.ru>
2023-07-21 22:34:18 +0300

    Снижение скорости UART до 1 Mbps из-за CH340

--- main.c
+++ main.c
@@ -12,13 +12,13 @@
 #define DEF_OCR2A_MICROS (F_CPU/500000 - 1) / 2 //Значение OCR2A для micros.
 
 typedef struct {
-  uint32_t micros;
+  uint64_t micros;
   uint8_t data;
   uint8_t notSent;
 } ADC_result;
 
 static volatile uint32_t micros_timer;
-static volatile ADC_result adc_result;
+static ADC_result adc_result;
 static char str_buf[32];
 
 ISR (TIMER2_COMPA_vect) {
@@ -31,14 +31,23 @@ ISR(ADC_vect)
 {
   //Здесь можем смело читать micros_timer, не запрещая прерывания
   adc_result.micros = micros_timer;
-  UART_puts_P("Read!\n");
   adc_result.data = ADCH;
   adc_result.notSent = 1;
 }
 
 void timer_init(void) {
    cli();
-   //Timer2 Init
+  //Timer2 Init
+  //Считать время в микросекундах, вызывая прервания 
+  //каждую микросекунду - плохая идея.
+  //Микроконтроллер будет все время занят подсчетом количества микросекунд
+  //Заметим, что регистр TCNT2 предоставляет прямой доступ, как для чтения, 
+  //так и для записи, к 8-разрядному счетчику.
+  //Его можно считать дополнительным байтом. 
+  //Количество микросекунд будем находить сдвигом счетчика переполнения
+  //таймера на 8 бит влево,
+  //а затем прибавлять значение регистра TCNT2.
+  //Необходимое значение делителя равно 16.
    micros_timer = 0;
    TCCR2B = 0b00000001;  // x1
    TCCR2A = 0b00000010;  //CTC mode
@@ -63,7 +72,12 @@ uint32_t micros() {
 void UART_Init() {
   /*Set baud rate */
   UBRR0H = 0;
-  UBRR0L = 0; /* Set maximum speed */
+  UBRR0L = 1; /* Здесь вынужден ставить скорость в 1 мегабод */
+  /* Мой CH340G не захотел работать со скоростью 2 Mbps */
+  /* Однако, в документации к этой микросхеме сказано, */
+  /* что поддерживается скорость до 2 Mbps */
+  /* Проверено, что при использовании ATmega16U2 в качестве USB - UART преобразователя */
+  /* Можно использовать скорость 2 Mbps */
   /*Set UCSR0A*/
   UCSR0A=0b00000000;
   UCSR0A |= (1 << U2X0); //set x2 speed for decreasing clock errors
@@ -97,7 +111,7 @@ void UART_puts_p(const char* s) {
 }
 
 void ADC_Init(void) {
-  
+  cli();
   ADCSRA |= (1<<ADEN) // Разрешение использования АЦП. Пока не включаем.
   |(1 << ADATE) //ADC Auto Trigger Enable
   |(1 << ADIE) //Разрешаем прерывания для АЦП
@@ -109,7 +123,8 @@ void ADC_Init(void) {
   ADMUX |= (1 << ADLAR); //Меняем порядок байтов результата АЦП.
   
   ADCSRB = 0x00; //режим Free Running
-  //ADCSRA |= (1<<ADEN); //Включили АЦП.
+  ADCSRA |= (1<<ADEN); //Включили АЦП.
+  sei();
   ADCSRA |= (1<<ADSC); //Начинаем преобразование
 }
 
@@ -120,21 +135,15 @@ int main() {
   adc_result.data = 0;
   adc_result.notSent = 0;
   UART_Init();
+  //timer_init(); 
   ADC_Init();
-  timer_init();
-  
-  
   UART_puts_P("Hello!\n");
   while(1) {
-    ltoa(micros(), str_buf, 10);
-    UART_puts(str_buf);
-    UART_putc('\n');
     if (adc_result.notSent) {
-      uint8_t oldSREG = SREG;
-      cli();
       adc_result.notSent = 0;
+      cli();
       ADC_result adc_result_c = adc_result;
-      SREG = oldSREG; 
+      sei();
       itoa(adc_result_c.micros, str_buf, 10);
       UART_puts(str_buf);
       UART_puts_P("    ");
